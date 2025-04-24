5819e4d401991f8578cf95029d1690174bcd95d6
t0mG33 <tommygalmiche@outlook.com>
2025-04-24 08:59:06 -0400

    Fixed updateQuantity and emoveItem functions

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,22 +7,20 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-        const { name, image, cost } = action.payload;
-        const existingItem = state.items.find(item => item.name === name);
+        const existingItem = state.items.find(item => item.name === action.payload.name.name);
         if (existingItem) {
             existingItem.quantity++;
         } else {
-            state.items.push({ name, image, cost, quantity: 1 });
+            state.items.push({ ...action.payload, quantity: 1 });
         }
     },
     removeItem: (state, action) => {
-        state.items = state.items.filter(item => item.name !== action.payload);
+        state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
-        const {name, quantity} = action.payload;
-        const itemFound = state.items.find(item => item.name === name);
-        if (itemFound) {
-            itemFound.quantity = quantity;
+        const itemToUpdate = state.items.find(item => item.name === action.payload.name);
+        if (itemToUpdate) {
+            itemToUpdate.quantity = action.payload.quantity;
         }
     },
   },
